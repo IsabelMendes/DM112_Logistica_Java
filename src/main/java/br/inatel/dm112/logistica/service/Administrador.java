c466cff21e10d2e85ae1dd872a6766bcde77f4bd
Isabel Mendes <isabelfrancinem@gmail.com>
2018-04-15 17:20:15 -0300

    Adicionando o serviço legado de e-mail

--- src/main/java/br/inatel/dm112/logistica/service/Administrador.java
+++ src/main/java/br/inatel/dm112/logistica/service/Administrador.java
@@ -1,11 +1,14 @@
 package br.inatel.dm112.logistica.service;
 
+import br.inatel.dm112.logistica.mail.MailImpl;
+import br.inatel.dm112.logistica.mail.MailService;
+import br.inatel.dm112.logistica.mail.MailStatusResponse;
 import br.inatel.dm112.logistica.model.Pedido;
 import br.inatel.dm112.logistica.repository.RepositorioPedido;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Calendar;
 import java.util.List;
 
 @RestController
@@ -17,5 +20,24 @@ public class Administrador {
     @GetMapping(path = "/pedidosEntegues")
     public List<Pedido> consultarPedidosEntregues() {
         return repositorioPedido.findByStatusEntregaTrue();
+    }
+
+    @PostMapping(path= "/enviarEmailDeEntrega/{id}")
+    public boolean enviarEmailDeEntrega (@PathVariable Long id){ //Path, pois não tem caracteres especiais.
+        Pedido pedido = repositorioPedido.findOne(id);
+        if (pedido != null && pedido.isStatusEntrega() == true && pedido.isEmailEnviado() == false){
+
+            //Consome o serviço legado de envio de e-mails do professor, que só envia boleto
+            MailService service = new MailService();
+            MailImpl port = service.getMailImplPort();
+            MailStatusResponse result = port.sendMail("robertorr9@gmail.com", "robertodm112", pedido.getEmail(),
+                    "Pedido entregue!".getBytes());
+
+            pedido.setEmailEnviado(true);
+            repositorioPedido.save(pedido);
+            return true;
+        } else{
+            return false;
+        }
     }
 }
