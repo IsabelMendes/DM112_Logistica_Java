8e8a88e2a8d9c9545bb7ce7adda566b2f51c6a58
Isabel Mendes <isabelfrancinem@gmail.com>
2018-04-19 21:27:55 -0300

    Alterando texto do corpo do e-mail.

--- src/main/java/br/inatel/dm112/logistica/service/Administrador.java
+++ src/main/java/br/inatel/dm112/logistica/service/Administrador.java
@@ -8,6 +8,7 @@ import br.inatel.dm112.logistica.repository.RepositorioPedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 import java.util.List;
 
@@ -28,15 +29,16 @@ public class Administrador {
     }
 
     @PostMapping(path= "/enviarEmailDeEntrega/{id}")
-    public boolean enviarEmailDeEntrega (@PathVariable Long id){ //Path, pois não tem caracteres especiais.
+    public boolean enviarEmailDeEntrega (@PathVariable Long id) throws UnsupportedEncodingException { //Path, pois não tem caracteres especiais.
         Pedido pedido = repositorioPedido.findOne(id);
         if (pedido != null && pedido.isStatusEntrega() == true && pedido.isEmailEnviado() == false){
 
             //Consome o serviço legado de envio de e-mails do professor, que só envia boleto
             MailService service = new MailService();
             MailImpl port = service.getMailImplPort();
+            String content = "Olá " + pedido.getNome() + "!\r\n Seu pedido acabou de ser entregue!";
             MailStatusResponse result = port.sendMail("robertorr9@gmail.com", "robertodm112", pedido.getEmail(),
-                    "Pedido entregue!".getBytes());
+                    content.getBytes("UTF-8"));
 
             pedido.setEmailEnviado(true);
             repositorioPedido.save(pedido);
